51ed2a14d02906258c1fd1e95972411a00dbc97d
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-05-01 02:29:39 -0700

    change server port to 8000

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => 
   console.log(`Server running on port ${PORT}`)
