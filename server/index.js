a2710b35d9d541adb687055e7f617112aab98d17
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-05-01 10:12:37 -0700

    use isAuthenticated provided by auth router

--- server/index.js
+++ server/index.js
@@ -33,9 +33,11 @@ const config = {
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 
-app.get("/", (req, res) => {
-  res.send("Hello, Talento!");
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
+
 
 app.listen(PORT, () => 
   console.log(`Server running on port ${PORT}`)
