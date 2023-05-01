f6bf648407403e326fb73f010c9853f2c350d8c8
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-05-01 02:31:08 -0700

    add get / route

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
-
 const app = express();
-
 const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, Talento!");
+});
 
 app.listen(PORT, () => 
   console.log(`Server running on port ${PORT}`)
