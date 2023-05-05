2f72aad8406e71c3315f4b0ec2a30613241d5972
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-05-05 19:38:47 -0400

    Create homepage

---
+++ client/src/components/Homepage/HomePage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './HomePage.css';
+
+const HomePage = () => {
+  const backgroundImage = `${process.env.PUBLIC_URL}/Background.png`;
+  const groupImage = `${process.env.PUBLIC_URL}/Group.png`;
+
+  return (
+    <div
+      className="home-page-container"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+      }}
+    >
+      <div className="center-text">
+        <h1 className="begin-journey-text">
+          Unlock Your <span className="highlighted">Dream</span> job with a couple swipes
+        </h1>
+        <p className="description-text">
+          Discover a world of opportunities with Talento, connecting job seekers and employers!
+        </p>
+      </div>
+      <img src={groupImage} alt="Group" className="group-image" />
+    </div>
+  );
+};
+
+export default HomePage;
