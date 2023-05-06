4587ed7599b11e495b0284e8720971c9a4ee345e
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-05-06 03:50:51 -0700

    include job seeker page in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import JobListing from './pages/JobListing';
+import JobSeeker from './pages/JobSeeker';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path="/job_listing" element={<JobListing />} />
+          <Route path="/job_seeker" element={<JobSeeker />} />
         </Routes>
     </Router>
   );
