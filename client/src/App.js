27cdaee0db23e8e5ae7f686eb3189b34db7b6af4
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-05-06 01:01:23 -0700

    use react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
-import HomePage from './components/Homepage/HomePage';
+import HomePage from './pages/HomePage';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Job_Listing from './pages/job_listing';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <HomePage />
-    </div>
+    // <div className="App">
+    //   <Navbar />
+    //   <HomePage />
+    // </div>
+    
+    <Router>
+        <Navbar />
+      <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path="/job_listing" element={<Job_Listing />} />
+        </Routes>
+    </Router>
   );
 }
 
