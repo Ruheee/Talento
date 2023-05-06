88acce48463f86e27ff82d942f1a0fc7a27956cc
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-05-06 03:16:46 -0700

    change class name to card-body

--- client/src/pages/JobListing.js
+++ client/src/pages/JobListing.js
@@ -8,17 +8,17 @@ const JobListing = () => {
     style={{
       backgroundImage: `url(${process.env.PUBLIC_URL}/Background.png)`,
     }}
-  >
-      <div className="card">
-        <img className="logo" src={`${process.env.PUBLIC_URL}/logo192.png`} width={135} />
-        <header className='company-name'>Company Name</header>
-        <section className='about'>About the job</section>
-        <article className='company-description'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget quam sodales, porta libero quis, sollicitudin massa. Nam ornare, dui ac hendrerit posuere, sapien justo mollis mi, eget hendrerit tortor augue in risus. Ut quis mauris eros. Sed rhoncus nisl non urna accumsan pharetra. Aliquam vel pulvinar libero.
-          <br />
-          <br />
-          Phasellus congue commodo libero a fringilla. Curabitur magna leo, sodales in dolor sed, blandit varius neque. Duis blandit lectus a augue congue, vel lobortis erat vestibulum. Proin a arcu id tortor accumsan tincidunt ac eu erat. Aliquam sit amet eros eu felis faucibus pulvinar. Vestibulum aliquam, ipsum ullamcorper blandit dictum, enim purus facilisis purus, id elementum erat massa a enim.
-        </article>
+    >
+        <div className="card-body">
+          <img className="logo" src={`${process.env.PUBLIC_URL}/logo192.png`} width={135} />
+          <header className='company-name'>Company Name</header>
+          <section className='about'>About the job</section>
+          <article className='company-description'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget quam sodales, porta libero quis, sollicitudin massa. Nam ornare, dui ac hendrerit posuere, sapien justo mollis mi, eget hendrerit tortor augue in risus. Ut quis mauris eros. Sed rhoncus nisl non urna accumsan pharetra. Aliquam vel pulvinar libero.
+            <br />
+            <br />
+            Phasellus congue commodo libero a fringilla. Curabitur magna leo, sodales in dolor sed, blandit varius neque. Duis blandit lectus a augue congue, vel lobortis erat vestibulum. Proin a arcu id tortor accumsan tincidunt ac eu erat. Aliquam sit amet eros eu felis faucibus pulvinar. Vestibulum aliquam, ipsum ullamcorper blandit dictum, enim purus facilisis purus, id elementum erat massa a enim.
+          </article>
       </div>
     </div>
   );
