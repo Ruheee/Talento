d8beec0d475f76ab54967d53e1ee6797360d8e16
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-05-07 05:07:12 -0700

    add fake seed data

---
+++ server/src/db/schema/careers.sql
@@ -0,0 +1,5 @@
+insert into careers (id, career) values (1, 'Law');
+insert into careers (id, career) values (2, 'Construction');
+insert into careers (id, career) values (3, 'Technology');
+insert into careers (id, career) values (4, 'Medical');
+insert into careers (id, career) values (5, 'Finance');
