883dd687d91c27bf590a3e7b0b3e2eb07e2206e8
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-05-08 16:36:27 -0400

    fix issues with pull

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import HomePage from "./components/Homepage/HomePage";
-import TestComponent from "./components/TestComponent";
 import "./styles/App.scss";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
@@ -10,10 +9,8 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* <HomePage /> */}
-      {/* Add your TestComponent here */}
-      <TestComponent />
-      {/* <Login /> */}
+      <HomePage />
+      <Login />
       <SignUp />
     </div>
   );
