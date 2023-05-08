6a8a155976e796e3ed394519ed24c9de744fa44c
Rui Dantas <ruidantas55@gmail.com>
2023-05-08 16:37:25 -0400

    Update message branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
 import React from "react";
+import "./styles/App.scss";
+
+
 import Navbar from "./components/Navbar/Navbar";
 import HomePage from "./components/Homepage/HomePage";
 import TestComponent from "./components/TestComponent";
-import "./styles/App.scss";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import Messages from "./pages/Messages"
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <HomePage />
+      {/* <Navbar />
+      <HomePage /> */}
       {/* Add your TestComponent here */}
-      <TestComponent />
+      {/* <TestComponent />
       <Login />
-      <SignUp />
+      <SignUp /> */}
+      <Messages />
     </div>
   );
 }
+
 
 export default App;
