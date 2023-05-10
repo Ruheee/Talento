4f1f82f14b546ba8593366aeaade3aa2d7037375
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-05-10 04:38:54 -0700

    compile all seeds into development

--- server/src/db/schema/favourites.sql
+++
@@ -1,5 +0,0 @@
-insert into favourites (id, job_seeker_id, job_listin_id) values (1, 3, 5);
-insert into favourites (id, job_seeker_id, job_listin_id) values (2, 3, 3);
-insert into favourites (id, job_seeker_id, job_listin_id) values (3, 4, 2);
-insert into favourites (id, job_seeker_id, job_listin_id) values (4, 1, 1);
-insert into favourites (id, job_seeker_id, job_listin_id) values (5, 1, 5);
