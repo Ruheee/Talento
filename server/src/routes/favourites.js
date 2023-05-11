27b840f429bf5bfb90e72d754c328b651bf871c9
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-05-10 17:35:15 -0700

    setup favourites route

--- server/src/routes/favourites.js
+++ server/src/routes/favourites.js
@@ -1,1 +1,13 @@
+const router = require("express").Router();
 
+module.exports = db => {
+  router.get("/favourites", (request, response) => {
+    db.query(
+      `SELECT * FROM favourites`
+    ).then(({ rows: favourites }) => {
+      response.json(favourites);
+    });
+  });
+
+  return router;
+};
