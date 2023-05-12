e0784fe1d5b75a0825cdcea0a990759e46ab7acb
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-05-12 12:22:29 -0700

    add query to select employer name

--- server/src/routes/job_listings.js
+++ server/src/routes/job_listings.js
@@ -3,7 +3,9 @@ const router = require("express").Router();
 module.exports = db => {
   router.get("/job_listings", (request, response) => {
     db.query(
-      `SELECT * FROM job_listings`
+      `SELECT t1.*, t2.company_name AS employer_name
+      FROM job_listings t1
+      JOIN employers t2 ON t2.id = t1.employer_id`
     ).then(({ rows: job_listings }) => {
       response.json(job_listings);
     });
