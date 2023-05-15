17b59c7446d0732995d88c0c3b82a5111c60b874
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-05-15 11:11:28 -0700

    include employer logo in query

--- server/src/routes/job_listings.js
+++ server/src/routes/job_listings.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 module.exports = db => {
   router.get("/job_listings", (request, response) => {
     db.query(
-      `SELECT t1.*, t2.company_name AS employer_name
+      `SELECT t1.*, t2.company_name AS employer_name, t2.company_logo AS employer_logo
       FROM job_listings t1
       JOIN employers t2 ON t2.id = t1.employer_id`
     ).then(({ rows: job_listings }) => {
