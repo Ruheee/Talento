e7a1ef71a95378b1e797e8bff60d64a674785a22
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-05-15 12:35:42 -0700

    separate job_seeker and job_listing API call

--- client/src/helpers/selectors.js
+++ client/src/helpers/selectors.js
@@ -1,24 +1,48 @@
 import axios from "axios";
 
-export function getUnmatchedItems(queryTypeAPI, matchesAPI) {
+export function getUnmatchedJobListings(jobListingsAPI, matchesAPI) {
   return new Promise((resolve, reject) => {
-    const unmatchedItemsArr = [];
+    const unmatchedJobListings = [];
 
-    axios.get(queryTypeAPI).then((items) => {
+    axios.get(jobListingsAPI).then((listings) => {
       axios.get(matchesAPI).then((matches) => {
-        const queryIDs = items.data.map((item) => item.id);
+        const jobListingIDs = listings.data.map((list) => list.id);
         const matchedJobListingIDs = matches.data
           .filter((match) => match.seeker_status === "true")
           .map((match) => match.job_listing_id);
 
         // extract only the job listings that have not been matched
-        queryIDs.forEach((queryID) => {
-          if (!matchedJobListingIDs.includes(queryID)) {
-            unmatchedItemsArr.push(items.data[queryID - 1]);
+        jobListingIDs.forEach((listID) => {
+          if (!matchedJobListingIDs.includes(listID)) {
+            unmatchedJobListings.push(listings.data[listID - 1]);
           }
         });
 
-        resolve(unmatchedItemsArr);
+        resolve(unmatchedJobListings);
+      });
+    });
+  });
+}
+
+export function getUnmatchedJobSeekers(jobSeekersAPI, matchesAPI) {
+  return new Promise((resolve, reject) => {
+    const unmatchedJobSeekers = [];
+
+    axios.get(jobSeekersAPI).then((seekers) => {
+      axios.get(matchesAPI).then((matches) => {
+        const jobSeekerIDs = seekers.data.map((seeker) => seeker.id);
+        const matchedJobSeekerIDs = matches.data
+          .filter((match) => match.employer_status === "true")
+          .map((match) => match.job_seeker_id);
+
+        // extract only the job listings that have not been matched
+        jobSeekerIDs.forEach((seekerID) => {
+          if (!matchedJobSeekerIDs.includes(seekerID)) {
+            unmatchedJobSeekers.push(seekers.data[seekerID - 1]);
+          }
+        });
+
+        resolve(unmatchedJobSeekers);
       });
     });
   });
