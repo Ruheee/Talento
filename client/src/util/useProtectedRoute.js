3936e3679284732b6952cc887576ea05da3d8c24
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-05-18 07:25:37 -0700

    remove unused setCookie

--- client/src/util/useProtectedRoute.js
+++ client/src/util/useProtectedRoute.js
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 
 export default function useProtectedRoute(route) {
-  const [cookies, setCookie] = useCookies(["user"]);
+  const [cookies] = useCookies(["user"]);
   const isAuthenticatedByCookie = cookies["auth0.spvFR3hekTCoajKUwC1DinYBuimjO18z.is.authenticated"];
 
   return isAuthenticatedByCookie ? route : <Navigate to="/login" replace />;
