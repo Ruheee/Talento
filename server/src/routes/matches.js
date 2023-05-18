3d82e9c960e963e198b9f69b4e83d3c57a99b6ff
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-05-18 11:51:16 -0700

    remove console.log

--- server/src/routes/matches.js
+++ server/src/routes/matches.js
@@ -11,8 +11,6 @@ module.exports = db => {
 
   router.post("/matches", (request, response) => {
 
-    console.log(request.body);
-
     if (request.body.employer_status === null) {
       return db.query(
         `INSERT INTO matches (job_listing_id, seeker_status)
