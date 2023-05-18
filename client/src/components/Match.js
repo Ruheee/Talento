def58aa50ccf991cd111a82a081556240264407c
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-05-18 04:33:20 -0700

    write match component

---
+++ client/src/components/Match.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../styles/Match.scss";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const Match = (props) => {
+  const Ronaldo = `${process.env.PUBLIC_URL}/Ronaldo.jpeg`;
+  const { user } = useAuth0();
+  const { picture } = user || {};
+  return (
+    <div className="match-container">
+      <header className="match-header">ITS A MATCH</header>
+      <p className="match-subheader">Send your match a message</p>
+      <div className="avatar-container">
+        <img src={picture} className="match-avatars"/>
+        <img src={Ronaldo} className="match-avatars" />  
+      </div>
+      <button className="send-message">SEND MESSAGE!</button>
+      <button className="keep-swiping" onClick={props.swipeMore}>Keep Swiping!</button>
+    </div>
+  )
+}
+
+export default Match;
