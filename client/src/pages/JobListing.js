b22a2e1fd53431df16208ae5575a6220c31b06ee
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-05-18 04:47:55 -0700

    change data dependencies

--- client/src/pages/JobListing.js
+++ client/src/pages/JobListing.js
@@ -1,7 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import '../styles/JobListing.scss';
+
 import classNames from "classnames";
 import { useSwipeable } from 'react-swipeable';
 import '../styles/JobListing.scss';
+
+import Match from "../components/Match";
+import {
+  getUnmatchedJobListings,
+  getUnmatchedJobSeekers,
+  randomIndex,
+} from "../helpers/selectors";
 
 const JobListing = () => {
   const [state, setState] = useState({
@@ -41,13 +51,13 @@ const JobListing = () => {
   const handlers = useSwipeable({
     onSwipedLeft: () => {
       console.log('Not Interested');
-      props.notInterested();
+      notInterested();
       setSwiping('left');
       setTimeout(() => setSwiping(''), 1000);
     },
     onSwipedRight: () => {
       console.log('Interested');
-      props.interested();
+      isInterested();
       setSwiping('right');
       setTimeout(() => setSwiping(''), 1000);
     },
@@ -121,73 +131,93 @@ const JobListing = () => {
   }, []);
 
   return (
-    <div
-      className="card-container"
-      style={{
-        backgroundImage: `url(${process.env.PUBLIC_URL}/Background.png)`,
-      }}>
-  
-      <button className="star" onClick={props.reset}>
-        Reset
-      </button>
-      <br />
-  
-      <div hidden={hiddenClass}>
-        <div 
-          className={`card-body ${swiping === 'right' ? 'swiping-right' : swiping === 'left' ? 'swiping-left' : ''} ${enlarged ? 'enlarge' : ''}`}
-          {...handlers} 
-          onClick={handleClick}
-        >
-          <img className="logo" src={props.data?.employer_logo} width={135} />
-          <header className="job-title">{props.data?.job_title}</header>
-          <header className="company-name">{props.data?.employer_name}</header>
-          <section className="content-header">About the job</section>
-          <article className="content-body">{props.data?.job_description}</article>
-          <div className="row-container">
-            <div>
-              <section className="content-header">Salary</section>
-              <article className="content-body">${props.data?.salary}</article>
-              <section className="content-header">Skills</section>
-              <article className="content-body">{props.data?.skills}</article>
+    <div>
+      <div
+        className="card-container"
+        style={{
+          backgroundImage: `url(${process.env.PUBLIC_URL}/Background.png)`,
+        }}>
+        <button className="star" onClick={resetDB}>
+          Reset
+        </button>
+        <br />
+
+        <div hidden={hiddenClass}>
+          <div
+            className={`card-body ${
+              swiping === "right"
+                ? "swiping-right"
+                : swiping === "left"
+                ? "swiping-left"
+                : ""
+            } ${enlarged ? "enlarge" : ""}`}
+            {...handlers}
+            onClick={handleClick}>
+            <img className="logo" src={jobListingData?.employer_logo} width={135} />
+            <header className="job-title">{jobListingData?.job_title}</header>
+            <header className="company-name">
+              {jobListingData?.employer_name}
+            </header>
+            <section className="content-header">About the job</section>
+            <article className="content-body">
+              {jobListingData?.job_description}
+            </article>
+            <div className="row-container">
+              <div>
+                <section className="content-header">Salary</section>
+                <article className="content-body">
+                  ${jobListingData?.salary}
+                </article>
+                <section className="content-header">Skills</section>
+                <article className="content-body">{jobListingData?.skills}</article>
+              </div>
+              <div>
+                <section className="content-header">Location</section>
+                <article className="content-body">
+                  {jobListingData?.city}, {jobListingData?.country}
+                </article>
+                <section className="content-header">Date Posted</section>
+                <article className="content-body">
+                  {jobListingData?.date_posted}
+                </article>
+              </div>
             </div>
-            <div>
-              <section className="content-header">Location</section>
-              <article className="content-body">
-                {props.data?.city}, {props.data?.country}
-              </article>
-              <section className="content-header">Date Posted</section>
-              <article className="content-body">{props.data?.date_posted}</article>
+
+          </div>
+            <div className="action-buttons">
+              <button
+                className="not-interested"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  notInterested();
+                }}>
+                Not Interested
+              </button>
+              <button
+                className="star"
+                onClick={
+                  (e) => {
+                    e.stopPropagation();
+                    toggleVisibility()
+                  }
+                }>
+                <i className="fas fa-star fa-lg"></i>
+              </button>
+              <button
+                className={`interested ${
+                  swiping === "right"
+                    ? "swiping-right"
+                    : swiping === "left"
+                    ? "swiping-left"
+                    : ""
+                }`}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  isInterested();
+                }}>
+                Interested
+              </button>
             </div>
-          </div>
-  
-          <div className="action-buttons">
-            <button 
-              className="not-interested" 
-              onClick={(e) => {
-                e.stopPropagation();
-                props.notInterested();
-              }}
-            >
-              Not Interested
-            </button>
-            <button 
-              className="star"
-              onClick={(e) => {
-                e.stopPropagation();
-              }}
-            >
-              <i className="fas fa-star fa-lg"></i>
-            </button>
-            <button 
-              className="interested" 
-              onClick={(e) => {
-                e.stopPropagation();
-                props.interested();
-              }}
-            >
-              Interested
-            </button>
-          </div>
         </div>
       </div>
   
