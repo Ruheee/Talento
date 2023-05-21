f541a75c029c9cdb902178db434fd7b063904768
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-05-21 12:59:34 -0700

    update Navbar links

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Navbar.css';
 import { useAuth0 } from "@auth0/auth0-react";
 import Logout from '../LogoutBtn';
+import { Link } from 'react-router-dom';
 
 
 const Navbar = () => {
@@ -11,18 +12,17 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="title-container">
-        <h1 className="title">Talento</h1>
+        <a href="/" className="title">Talento</a>
       </div>
-      <div className="search-container">
+      {/* <div className="search-container">
         <input type="text" className="search-input" placeholder="Search..." />
-      </div>
+      </div> */}
       <div className="buttons-container">
-        <a href="#" className="nav-link">Careers</a>
-        <a href="https://localhost:3006/messages" className="nav-link">Messages</a>
-        <a href="#" className="nav-link">Freelance</a>
+        <a href="/job_listing" className="nav-link">Find Jobs</a>
+        <a href="/messages" className="nav-link">Messages</a>
         <img src={picture} className='user-avatar'/>
         <span className="nav-link">{email}</span>
-        {!isAuthenticated ? <a href="https://localhost:3006/login" className="login-button">Login</a> : <Logout />}
+        {!isAuthenticated ? <a href="/login" className="login-button">Login</a> : <Logout />}
       </div>
     </div>
   );
