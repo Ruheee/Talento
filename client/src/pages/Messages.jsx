9a8c27f0ada814d89b2b8fd2e2ab63711c3add4f
Rui Dantas <ruidantas55@gmail.com>
2023-05-22 12:20:47 -0400

    Fix login

--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar/Navbar";
 import "../styles/Messages.scss";
 import "../styles/index.scss";
 import MatchList from "../components/MatchList";
-import Login from "../pages/LogIn";
+import Login from "../pages/Login";
 import MessageList from "../components/MessageList";
 
 // Import from react library
@@ -25,8 +25,6 @@ export default function Messages() {
   const [isTrue, setIsTrue] = useState(false);
   const [newMessage, setNewMessage] = useState(false);
   const [userID, setUserID] = useState("0");
-
-  const avatar = `${process.env.PUBLIC_URL}/avatar.jpg`;
 
   console.log(newMessage)
 
@@ -188,7 +186,7 @@ export default function Messages() {
                     return (
                       <div id="sender_message">
                         <div className="message--container sender_msg">
-                          <img src={avatar} alt={user.name} />
+                          <img src={user.picture} alt={user.name} />
                           <div className="username--message">
                             <p>
                               {(message.user_id === "1" && "Seeker") ||
